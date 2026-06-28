d375657d742fb6ce0d0c56aa4dfccbd9366106d7
champiw <sergio.b.cast@gmail.com>
2026-06-28 16:54:25 +0200

    added config comment

--- modules/programs/jellyfin-tui.nix
+++ modules/programs/jellyfin-tui.nix
@@ -4,4 +4,18 @@
    environment.systemPackages = with pkgs; [
       jellyfin-tui
    ];
+
+   # Config file in ~/.config/jellyfin-tui/config.yaml
+   #
+   # Minumum config:
+   # 
+   # servers:
+   # - name: Home Server
+   # password: --password--
+   # url: --server-url--
+   # username: --username--
+   # 
+   # Optional config:
+   # 
+   # Grab from https://github.com/dhonus/jellyfin-tui until discord status part.
 }
