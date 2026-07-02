9583b7feac6b238125f887f4c8f1f98956026e7b
champiw <sergio.b.cast@gmail.com>
2026-07-02 16:14:41 +0200

    vial changes

--- modules/programs/vial.nix
+++ modules/programs/vial.nix
@@ -1,9 +1,18 @@
 { pkgs, ... }:
 
 {
-    home-manager.users.champi = {
-      home.packages = with pkgs; [
-	vial
-      ];
-    };
+  home-manager.users.champi = {
+    home.packages = with pkgs; [
+      vial
+    ];
+  };
+
+  hardware.keyboard.qmk.enable = true;
+
+  services.udev.packages = with pkgs; [ qmk-udev-rules ];
+
+  users.users.champi.extraGroups = [
+    "input"
+    "uinput"
+  ];
 }
