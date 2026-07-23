1e1ded66ae2e35a476ef0e32212f5b67eae17101
champiw <sergio.b.cast@gmail.com>
2026-07-24 00:22:04 +0200

    forgot to add nvim nix file

--- modules/programs/nvim.nix
+++ modules/programs/nvim.nix
@@ -1,20 +1,34 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
 {
-   home-manager.users.champi = { config, ... }: {
-      home.packages = with pkgs; [
-        neovim
-		#ripgrep
-		#nil
-		#nixpkgs-fmt
-		#nodejs
-		#gcc
-      ];
+	home-manager.users.champi = { config, pkgs, ... }: {
+		home.packages = with pkgs; [
+			neovim
 
-      # Symlink nvim folder with lua config
-      xdg.configFile."nvim" = {
-        source = config.lib.file.mkOutOfStoreSymlink 
-		"${config.home.homeDirectory}/.dotfiles/modules/programs/nvim";
-      };
-   };
+			# LSPs
+			nixd
+			lua-language-server
+
+			vscode-langservers-extracted
+			tailwindcss-language-server
+			svelte-language-server
+			typescript
+			typescript-language-server
+
+			docker-language-server
+			docker-compose-language-service
+
+			# tools & dependencies
+			ripgrep
+			fd
+			tree-sitter
+			gcc
+		];
+
+		# Symlink nvim folder with lua config
+		xdg.configFile."nvim" = {
+			source = config.lib.file.mkOutOfStoreSymlink 
+				"${config.home.homeDirectory}/.dotfiles/modules/programs/nvim";
+		};
+	};
 }
