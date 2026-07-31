ce44d3ad5c9fff51fdd73e80d6c044581c38c246
champiw <sergio.b.cast@gmail.com>
2026-08-01 01:06:22 +0200

    moved vim pack commands to other file and added go lsp

--- modules/programs/nvim/init.lua
+++ modules/programs/nvim/init.lua
@@ -1,3 +1,5 @@
+require("commands")
+
 vim.o.number = true
 vim.o.cursorline = true
 vim.o.relativenumber = true
@@ -36,6 +38,7 @@ vim.cmd.colorscheme("gruvbox")
 local lsp_servers = {
 	"nixd",
 	"lua_ls",
+	"gopls",
 
 	"html",
 	"cssls",
@@ -140,83 +143,3 @@ require("trouble").setup({
 })
 
 vim.keymap.set("n", "<leader>q", "<CMD>Trouble diagnostics toggle<CR>")
-
-
--- Package management ----------------------------------------------------------
--- :PackUpdate - Updates all plugins
--- :PackUpdate blink.cmp - Updates only one
--- :PackDelete oil.nvim - Removes a plugin
--- :PackClean - Removes installed plugins not present in vim.pack.add()
-
-local function complete_packages(arg_lead)
-	arg_lead = arg_lead or ""
-
-	return vim.iter(vim.pack.get())
-		:map(function(plugin)
-			return plugin.spec.name
-		end)
-		:filter(function(name)
-			return vim.startswith(name, arg_lead)
-		end)
-		:totable()
-end
-
-vim.api.nvim_create_user_command("PackUpdate", function(info)
-	if #info.fargs ~= 0 then
-		vim.pack.update(info.fargs, { force = info.bang })
-	else
-		local choice = vim.fn.confirm(
-			"Do you want to update ALL packages?",
-			"&Yes\n&No",
-			2
-		)
-
-		if choice == 1 then
-			vim.notify("Updating all packages...", vim.log.levels.INFO)
-			vim.pack.update(nil, { force = info.bang })
-		else
-			vim.notify("Update cancelled.", vim.log.levels.WARN)
-		end
-	end
-end, {
-	desc = "Update vim.pack packages",
-	nargs = "*",
-	bang = true,
-	complete = complete_packages,
-})
-
-vim.api.nvim_create_user_command("PackDelete", function(info)
-	vim.pack.del(info.fargs, { force = info.bang })
-end, {
-	desc = "Delete vim.pack packages",
-	nargs = "+",
-	bang = true,
-	complete = complete_packages,
-})
-
-vim.api.nvim_create_user_command("PackClean", function()
-	local unused = {}
-
-	for _, plugin in ipairs(vim.pack.get()) do
-		if not plugin.active then
-			table.insert(unused, plugin.spec.name)
-		end
-	end
-
-	if #unused == 0 then
-		vim.notify("No unused packages.", vim.log.levels.INFO)
-		return
-	end
-
-	local choice = vim.fn.confirm(
-		"Delete unused packages?",
-		"&Yes\n&No",
-		2
-	)
-
-	if choice == 1 then
-		vim.pack.del(unused)
-	end
-end, {
-	desc = "Delete inactive vim.pack packages",
-})
